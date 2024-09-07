0d5dd49491de8ce6d740d77611d492da7e309e00
Jonathan Trailor <JJTrailor@gmail.com>
2024-09-07 13:43:14 -0400

    Added header and core pages

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <section id="experience" className="min-h-screen p-8">
+      <h1 className="text-4xl">Experience</h1>
+      <p>Content for Experience</p>
+    </section>
+  );
+}
+
+export default Experience;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <section id="intro" className="min-h-screen p-8">
+      <h1 className="text-4xl">Intro</h1>
+      <p>Content for Intro</p>
+    </section>
+  );
+}
+
+export default Intro;
